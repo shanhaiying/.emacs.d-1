7e05a8482d3f47d3997f4034c9e48f5498649b99
zhenglinj <zhenglinj@hotmail.com>
2014-05-15 20:13:29 +0800

    maked *_flymake.* ("Makefile" projects) under ./tmp

--- init-flymake.el
+++ init-flymake.el
@@ -13,4 +13,40 @@ Return nil if we cannot, non-nil if we can."
        (if (and file-name (flymake-get-init-function file-name)) t nil))
      ))
 
+;; http://blog.urth.org/2011/06/02/flymake-versus-the-catalyst-restarter/
+;; This function replaced the use of “flymake-create-temp-inplace” with “flymake-create-temp-intemp”, defined below.
+;; It tells flymake to make its files in a temp directory. It make all *_flymake.* files in a temp directory.
+(defun flymake-create-temp-intemp (file-name prefix)
+  "Return file name in temporary directory for checking
+   FILE-NAME. This is a replacement for
+   `flymake-create-temp-inplace'. The difference is that it gives
+   a file name in `temporary-file-directory' instead of the same
+   directory as FILE-NAME.
+
+   For the use of PREFIX see that function.
+
+   Note that not making the temporary file in another directory
+   \(like here) will not if the file you are checking depends on
+   relative paths to other files \(for the type of checks flymake
+   makes)."
+  (unless (stringp file-name)
+    (error "Invalid file-name"))
+  (or prefix
+      (setq prefix "flymake"))
+  (let* ((name (concat
+                (file-name-nondirectory
+                 (file-name-sans-extension file-name))
+                "_" prefix))
+         (ext  (concat "." (file-name-extension file-name)))
+         (temp-name (make-temp-file name nil ext))
+         )
+    (flymake-log 3 "create-temp-intemp: file=%s temp=%s" file-name temp-name)
+    temp-name))
+
+(setq temporary-file-directory "~/.emacs.d/tmp/")
+
+;; Redefine this function in /usr/local/share/emacs/24.3/lisp/progmodes/flymake.el.gz
+(defun flymake-simple-make-init ()
+  (flymake-simple-make-init-impl 'flymake-create-temp-intemp t t "Makefile" 'flymake-get-make-cmdline))
+
 (provide 'init-flymake)
