3dfb4cbef0ed450a26f8e5f872a43b9a858a7071
zhenglinj <zhenglinj@hotmail.com>
2014-12-04 13:50:08 +0800

    增加 go-autocomplete

--- init-go-mode.el
+++ init-go-mode.el
@@ -1,26 +1,93 @@
 (require 'go-mode)
+(require 'go-autocomplete)
 
+(speedbar-add-supported-extension ".go")
+(add-hook 'go-mode-hook
+	  (lambda ()
+	    ;; gocode
+	    (company-mode -1)
+	    (auto-complete-mode 1)
+	    (setq ac-sources '(ac-source-go))
+	    ;; (call-process "gocode" nil nil nil "-s")
+
+	    ;; ;; Imenu & Speedbar
+	    ;; (setq imenu-generic-expression
+	    ;; 	  '(("type" "^type *\\([^ \t\n\r\f]*\\)" 1)
+	    ;; 	    ("func" "^func *\\(.*\\) {" 1)))
+	    ;; (imenu-add-to-menubar "Index")
+	    ;; ;; Outline mode
+	    ;; (make-local-variable 'outline-regexp)
+	    ;; (setq outline-regexp "//\\.\\|//[^\r\n\f][^\r\n\f]\\|pack\\|func\\|impo\\|cons\\|var.\\|type\\|\t\t*....")
+	    ;; (outline-minor-mode 1)
+	    ;; (local-set-key "\M-a" 'outline-previous-visible-heading)
+	    ;; (local-set-key "\M-e" 'outline-next-visible-heading)
+	    ;; ;; Menu bar
+	    ;; (require 'easymenu)
+	    ;; (defconst go-hooked-menu
+	    ;;   '("Go tools"
+	    ;; 	["Go run buffer" go t]
+	    ;; 	["Go reformat buffer" go-fmt-buffer t]
+	    ;; 	["Go check buffer" go-fix-buffer t]))
+	    ;; (easy-menu-define
+	    ;;   go-added-menu
+	    ;;   (current-local-map)
+	    ;;   "Go tools"
+	    ;;   go-hooked-menu)
+
+	    ;; Other
+	    (setq show-trailing-whitespace t)
+	    ))
+
+;; helper function
 (defun go-run-buffer()
+  "run current buffer"
   (interactive)
   (shell-command (concat "go run " (buffer-name))))
 
-(defun go-kill()
+;; helper function
+(defun go-fmt-buffer ()
+  "run gofmt on current buffer"
   (interactive)
-  (if (go-mode-in-string)
-      (paredit-kill-line-in-string)
-    (paredit-kill)))
+  (if buffer-read-only
+      (progn
+        (ding)
+        (message "Buffer is read only"))
+    (let ((p (line-number-at-pos))
+          (filename (buffer-file-name))
+          (old-max-mini-window-height max-mini-window-height))
+      (show-all)
+      (if (get-buffer "*Go Reformat Errors*")
+          (progn
+            (delete-windows-on "*Go Reformat Errors*")
+            (kill-buffer "*Go Reformat Errors*")))
+      (setq max-mini-window-height 1)
+      (if (= 0 (shell-command-on-region (point-min) (point-max) "gofmt" "*Go Reformat Output*" nil "*Go Reformat Errors*" t))
+          (progn
+            (erase-buffer)
+            (insert-buffer-substring "*Go Reformat Output*")
+            (goto-char (point-min))
+            (forward-line (1- p)))
+        (with-current-buffer "*Go Reformat Errors*"
+          (progn
+            (goto-char (point-min))
+            (while (re-search-forward "<standard input>" nil t)
+              (replace-match filename))
+            (goto-char (point-min))
+            (compilation-mode))))
+      (setq max-mini-window-height old-max-mini-window-height)
+      (delete-windows-on "*Go Reformat Output*")
+      (kill-buffer "*Go Reformat Output*"))))
 
-(defun go-backward-delete()
+;; helper function
+(defun go-fix-buffer ()
+  "run gofix on current buffer"
   (interactive)
-  (if (go-mode-in-string)
-      (paredit-backward-delete-in-string)
-    (paredit-backward-delete)))
+  (show-all)
+  (shell-command-on-region (point-min) (point-max) "go tool fix -diff"))
 
 (define-key go-mode-map (kbd "C-c C-c") #'go-run-buffer)
 (define-key go-mode-map (kbd "C-c C-f") #'gofmt)
 (define-key go-mode-map (kbd "C-c C-d") #'godoc)
 (define-key go-mode-map (kbd "C-c C-a") #'go-import-add)
-(define-key go-mode-map (kbd "C-k") #'go-kill)
-(define-key go-mode-map (kbd "M-o") #'go-backward-delete)
 
 (provide 'init-go-mode)
